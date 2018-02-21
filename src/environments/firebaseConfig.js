0aa67071527950ac51156b51faea8be4ef15b434
Thomas McClellan <thomasmcclellan87@gmail.com>
2018-02-21 11:32:21 -0500

    Added comments

--- src/environments/firebaseConfig.js
+++ src/environments/firebaseConfig.js
@@ -1,3 +1,6 @@
+// Access to firebase for auth/db => might go this way, use Postgres, or different option altogether
+
+
 export default {
   apiKey: "AIzaSyB-mzrYn3Kl3i5oy-2N-NL8yOjL5R8oKfg",
   authDomain: "todahgive.firebaseapp.com",
